a01aa74a3eec063787cd8b6d541681f87b577414
Yan Kazymyr <kazymyr.yan@gmail.com>
2025-10-05 21:25:39 +0300

    Solution

--- src/Types/Good.ts
+++ src/Types/Good.ts
@@ -1,1 +1,2 @@
+// type of goodsFromServer
 export type Good = string;
--- src/Types/SortType.ts
+++ src/Types/SortType.ts
@@ -1,5 +1,5 @@
 export enum SortType {
+  Reset = 'reset',
   Alphabet = 'alphabet',
   Length = 'length',
-  Reset = 'reset',
 }
